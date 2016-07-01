7a6a08e8b310a5474d8b7f3df469602f962ffc73
Chengzhi Jia <j.chengzhi1989@gmail.com>
2016-06-30 22:03:28 -0700

    Add test Framework

--- CZBLEControl/CZBLEControl/AppDelegate.swift
+++ CZBLEControl/CZBLEControl/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -22,6 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UINavigationBar.appearance().tintColor = UIColor.whiteColor()
         UIBarButtonItem.appearance().setBackButtonTitlePositionAdjustment(UIOffsetMake(0.0, -80.0), forBarMetrics: .Default)
 
+        Fabric.with([Crashlytics.self])
+        
         return true
     }
 
