e436b2989be1f8e7f7054fec629b7ffbfd654f06
Chengzhi Jia <j.chengzhi1989@gmail.com>
2016-12-07 00:50:18 -0800

    Finish English localization settings

--- CZBLEControl/CZBLEControl/DropBoxManager.swift
+++ CZBLEControl/CZBLEControl/DropBoxManager.swift
@@ -44,16 +44,16 @@ class DropBoxManager: NSObject {
         if let data = tupleJoinStr(dataArray).data(using: String.Encoding.utf8) {
             uploadData(title, data: data, completionHandler: completionHandler)
         } else {
-            completionHandler(false, "Data cannot be transferred to file")
+            completionHandler(false, NSLocalizedString("Data cannot be transferred to file", comment: ""))
         }
     }
     
     func saveWriteAndValueData(_ title: String, writeArray: [(String, String)], valueArray: [(String, String)], completionHandler: @escaping statusMessageHandler) {
-        let dataStr = "Write Value\n\n" + tupleJoinStr(writeArray) + "Read Value\n\n" + tupleJoinStr(valueArray)
+        let dataStr = NSLocalizedString("Write Value", comment: "") + "\n\n" + tupleJoinStr(writeArray) + NSLocalizedString("Read Value", comment: "") + "\n\n" +  tupleJoinStr(valueArray)
         if let data = dataStr.data(using: String.Encoding.utf8) {
             uploadData(title, data: data, completionHandler: completionHandler)
         } else {
-            completionHandler(false, "Data cannot be transferred to file")
+            completionHandler(false, NSLocalizedString("Data cannot be transferred to file", comment: ""))
         }
     }
     
@@ -62,11 +62,11 @@ class DropBoxManager: NSObject {
             if success {
                 DropboxClientsManager.authorizedClient?.files.upload(path: "/\(kFolderName)/\(title).txt", input: data).response(completionHandler: { (metaData, error) in
                     if error != nil {
-                        completionHandler(false, "Upload file failed")
+                        completionHandler(false, NSLocalizedString("Upload file failed", comment: ""))
                     } else if metaData != nil {
-                        completionHandler(true, "Upload file successfully")
+                        completionHandler(true, NSLocalizedString("Upload file successfully", comment: ""))
                     } else {
-                        completionHandler(false, "File data unavailable")
+                        completionHandler(false, NSLocalizedString("Data unavailable", comment: ""))
                     }
                 })
             } else {
@@ -80,31 +80,31 @@ class DropBoxManager: NSObject {
     fileprivate func createFolder(_ completionHandler: @escaping statusMessageHandler) {
         DropboxClientsManager.authorizedClient?.files.listFolder(path: "", recursive: false, includeMediaInfo: false, includeDeleted: false, includeHasExplicitSharedMembers: false).response(completionHandler: { (response, error) in
             if error != nil {
-                completionHandler(false, "Error when chenck file list")
+                completionHandler(false, NSLocalizedString("Failed to download file", comment: ""))
             } else if let entries = response?.entries {
                 var folderExisted = false
                 for entry in entries {
                     if entry.name == kFolderName {
                         self.DropboxFolder = entry
                         folderExisted = true
-                        completionHandler(true, "Folder existed")
+                        completionHandler(true, NSLocalizedString("Correct folder found", comment: ""))
                         return
                     }
                 }
                 if !folderExisted {
                     DropboxClientsManager.authorizedClient?.files.createFolder(path: "/\(kFolderName)").response(completionHandler: { (fileMetaData, error) in
                         if error != nil {
-                            completionHandler(false, "Create folder failed")
+                            completionHandler(false, NSLocalizedString("Create folder failed", comment: ""))
                         } else if fileMetaData != nil {
                             self.DropboxFolder = fileMetaData!
-                            completionHandler(true, "Create folder successfully")
+                            completionHandler(true, NSLocalizedString("Create folder successfully", comment: ""))
                         } else {
-                            completionHandler(false, "Folder data unavailable")
+                            completionHandler(false, NSLocalizedString("Data unavailable", comment: ""))
                         }
                     })
                 }
             } else {
-                completionHandler(false, "No data available")
+                completionHandler(false, NSLocalizedString("Data unavailable", comment: ""))
             }
         })
     }
@@ -112,14 +112,14 @@ class DropBoxManager: NSObject {
     func loadFileList(_ completionHandler: @escaping (_ success: Bool, _ dataArray: [Files.Metadata]?, _ errorMessage: String?) -> Void) {
         DropboxClientsManager.authorizedClient?.files.listFolder(path: "/\(kFolderName)", recursive: false, includeMediaInfo: false, includeDeleted: false, includeHasExplicitSharedMembers: false).response(completionHandler: { (response, error) in
             if error != nil {
-                completionHandler(false, nil, "Error when chenck file list")
+                completionHandler(false, nil, NSLocalizedString("No file to show", comment: ""))
             } else if let filesList = response?.entries {
                 let validTextList = filesList.filter({ (file) -> Bool in
                     file.name.hasSuffix(".txt")
                 })
-                completionHandler(true, validTextList, "Successfully get files list")
+                completionHandler(true, validTextList, NSLocalizedString("Load data successfully", comment: ""))
             } else {
-                completionHandler(false, nil, "Error when chenck file list")
+                completionHandler(false, nil, NSLocalizedString("Cannot query files", comment: ""))
             }
         })
     }
@@ -130,19 +130,19 @@ class DropBoxManager: NSObject {
             return NSURL(fileURLWithPath: pathStr) as URL
         }).response(completionHandler: { (response, error) in
             if error != nil {
-                completionHandler(false, nil, "Failed to read file content")
+                completionHandler(false, nil, NSLocalizedString("Load data failed", comment: ""))
             } else if let (_, url) = response {
                 if let data = NSData(contentsOf: url) {
                     if let dataString = NSString(data: data as Data, encoding: String.Encoding.utf8.rawValue) {
-                        completionHandler(true, dataString.parseToDataTableView(), "Parse file content successfully")
+                        completionHandler(true, dataString.parseToDataTableView(), NSLocalizedString("Load data successfully", comment: ""))
                     } else {
-                        completionHandler(false, nil, "Failed to parse file content")
+                        completionHandler(false, nil, NSLocalizedString("Load data failed", comment: ""))
                     }
                 } else {
-                    completionHandler(false, nil, "Failed to transfer file content")
+                    completionHandler(false, nil, NSLocalizedString("Data cannot be transferred to file", comment: ""))
                 }
             } else {
-                completionHandler(false, nil, "Failed to read file content")
+                completionHandler(false, nil, NSLocalizedString("Load data failed", comment: ""))
             }
         })
     }
@@ -150,9 +150,9 @@ class DropBoxManager: NSObject {
     func deleteFile(_ fileObj: Files.Metadata, completionHandler: @escaping statusMessageHandler) {
         DropboxClientsManager.authorizedClient?.files.delete(path: "/\(kFolderName)/\(fileObj.name)").response(completionHandler: { (response, error) in
             if error != nil {
-                completionHandler(false, "Failed to delete the file")
+                completionHandler(false, NSLocalizedString("Failed to delete the file", comment: ""))
             } else {
-                completionHandler(true, "Successfully delete the file")
+                completionHandler(true, NSLocalizedString("Successfully delete the file", comment: ""))
             }
         })
     }
